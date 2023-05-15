fae3c932b6479da4079f26848d28bfcc81bba9a1
Kevin Koech <kevinkoech@proton.me>
2023-05-15 11:01:17 +0300

    added comments to show what each command does

--- mp4_to_mp3.sh
+++ mp4_to_mp3.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # this script converts all mp4 files to mp3
-# git clone to your dir and run it
+# git clone to your dir and run it ./mp4_to_mp3.sh
+# vn: disable video recording during the conversion.
+# ar: set audio sampling rate in Hz.
+# ab: set the audio bitrate.
+# ac: set the number of audio channels.
+# -f: format.
 
-echo off
-# mkdir comversions
-for %%a in (".mp4")
-do ffmpeg -i "%%a" -b:a 192K -vn "conversions\%%~na.mp3"
-pause
+mkdir conversions
+for i in *.mp4; do ffmpeg -i "$i" -f mp3 -ab 192k -vn -ar 44100 -ac 2 conversions/"${i%.*}.mp3"; done
+
